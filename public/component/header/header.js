8ab5af915d083b38054bf932d42258accc8273f7
yeiin <88276674+yeiin@users.noreply.github.com>
2025-10-22 09:28:04 +0900

    feat: header profile 수정

--- public/component/header/header.js
+++ public/component/header/header.js
@@ -19,6 +19,39 @@ export function renderHeader({ back = false, profile = false, image = null } = {
       });
       profileImage.appendChild(img);
     }
-    
+    renderHeaderProfile();
   }
+}
+
+
+export function renderHeaderProfile(){
+  const profileBtn = document.getElementById("header-profile-image");
+  const profileMenu = document.getElementById("profile-menu");
+
+  profileBtn.addEventListener("click", () => {
+    profileMenu.classList.toggle("hidden");
+  });
+
+  document.addEventListener("click", (event) => {
+    if (!profileBtn.contains(event.target) && !profileMenu.contains(event.target)) {
+      profileMenu.classList.add("hidden");
+    }
+  });
+
+  const profileModifyBtn = document.getElementById("header-profile-modify-button");
+  profileModifyBtn.addEventListener("click", () => {
+    location.href = "/pages/profile/profile.html"
+  });
+
+  const passwordModifyBtn = document.getElementById("header-password-modify-button");
+  passwordModifyBtn.addEventListener("click", () => {
+    location.href = "/pages/password/password.html"
+  });
+
+  const logoutBtn = document.getElementById("logout-button");
+  logoutBtn.addEventListener("click", (event) => {
+   
+  });
+
+
 }
