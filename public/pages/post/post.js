fd38d799e2a75ae8c644ff42af32f8d6d37b6e60
yeiin <88276674+yeiin@users.noreply.github.com>
2025-10-23 09:32:32 +0900

    feat: post

--- public/pages/post/post.js
+++ public/pages/post/post.js
@@ -6,28 +6,33 @@ import { commentApi } from "/api/comment/commentApi.js";
 let isLoading = false;
 let lastSeenId = null;
 let fetchComplete = false;
+let isLike = false;
+
+const params = new URLSearchParams(location.search);
+const postId = params.get('postId');
+
+let commentSubmitBtn;
+let comment;
 
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const params = new URLSearchParams(location.search);
-  const postId = params.get('postId');
-
+  
   await componentLoader("header","/component/header/header", true, true, null);
   renderHeader({ back: true, profile: true });
 
-  await fetchPost(postId);
+  await fetchPost();
 
   await componentLoader("comment-submit-button", "/component/button/round-button/round-button", true, false, {
     text: "댓글 등록"
   });
-  addEventListenerToCommentSubmitButton(postId);
+  addEventListenerToComment();
 
-  await fetchComments(postId);
-  addEventListenerToPostModifiyButton();
-  addEventListenerToPostDeleteButton(postId);
+  commentSubmitBtn = document.querySelector("#comment-submit-button round-button .round-button");
+  comment = document.querySelector("#comment-text");
 
-  addEventListenerToScroll(postId);
-  addEventListenerToLikeButton();
+  await fetchComments();
+  addEventListenerToOwnButtons();
+  addEventListenerToScroll();
 
   window.addEventListener("pageshow", (event) => {
     if (event.persisted && sessionStorage.getItem("shouldReload")==="true") {
@@ -37,33 +42,56 @@ document.addEventListener("DOMContentLoaded", async () => {
   });
 });
 
-function addEventListenerToCommentSubmitButton(postId){
-  const commentSubmitBtn = document.querySelector("#comment-submit-button");
-  
-  commentSubmitBtn.addEventListener("click", async () => {
-    const comment = document.querySelector("#comment-text");
+function addEventListenerToComment(){
+  commentSubmitBtn.disabled = true;
 
-    if(comment == null && comment.value.length == 0) return;
+  comment.addEventListener("input", () =>{
+    if(comment == null && comment.value.length == 0) commentSubmitBtn.disabled = true;
+    else commentSubmitBtn.disabled = false;
+  });
+
+  commentSubmitBtn.addEventListener("click", async () => {
+    if(commentSubmitBtn.disabled) return;
 
     try{
-      const response = await commentApi.postComment(postId,{contents: comment.value});
+      const commentId = sessionStorage.getItem("commentId");
+      let response;
+
+      if(!commentId){
+        response = await commentApi.postComment(postId,{contents: comment.value});
+      }else{
+        console.log(commentId);
+        response = await commentApi.patchComment(postId, commentId, {contents: comment.value});
+      }
 
       if(!response.ok){
-        alert("댓글 작성에 실패했습니다.");
+        if(response.status === 413){
+          alert("댓글이 너무 깁니다.");
+        }else if(response.status === 404){
+          alert("게시글을 찾을 수 없습니다.");
+          history.back();
+        }else{
+          alert("댓글 작성에 실패했습니다.");
+        }
         return;
       }
 
     }catch(e){
 
     }finally{
+      sessionStorage.removeItem("commentId");
       window.location.reload();
     }
   });
-
 }
 
-function addEventListenerToPostDeleteButton(postId){
+function addEventListenerToOwnButtons(){
+  const postModifyBtn = document.querySelector("#post-modify-button");
   const postDeleteBtn = document.querySelector("#post-delete-button");
+
+  postModifyBtn.addEventListener("click", async () => {  
+    window.location.href = "/pages/writing/writing.html?isNew=false";
+  });
 
   postDeleteBtn.addEventListener("click", async () => {  
     await componentLoader("dialog", "/component/dialog/dialog", true, false, {
@@ -84,7 +112,13 @@ function addEventListenerToPostDeleteButton(postId){
         const response = await postApi.deletePost(postId);
 
         if(!response.ok){
-          alert("게시물 삭제 실패");
+          if(response.status === 403){
+            alert("삭제가 할 수 없습니다.");
+          }if(response.status === 404){
+            alert("게시물을 찾을 수 없습니다.");
+          }else{
+            alert("게시물 삭제 실패");
+          } 
           return;
         }
         
@@ -101,22 +135,11 @@ function addEventListenerToPostDeleteButton(postId){
           }
         });
       }
-      
     });
   });
 }
 
-function addEventListenerToPostModifiyButton(){
-
-  const postModifyBtn = document.querySelector("#post-modify-button");
-
-  postModifyBtn.addEventListener("click", async () => {  
-    window.location.href = "/pages/writing/writing.html?isNew=false";
-  });
-}
-
-
-function addEventListenerToScroll(postId){
+function addEventListenerToScroll(){
   window.addEventListener("scroll", async () => {
     if (fetchComplete || isLoading) return;
 
@@ -131,15 +154,19 @@ function addEventListenerToScroll(postId){
 
 }
 
-async function fetchPost(postId){
+async function fetchPost(){
   try{
     const response = await postApi.getPost(postId);
 
     if(!response.ok){
-      console.log(response.status);
+      if(response.status === 404){
+        alert("게시물을 찾을 수 없습니다.");
+        history.back();
+      }
       return;
     }
 
+    //내용 채우기
     const result = await response.json();
     
     const title = document.getElementById("post-title");
@@ -154,36 +181,97 @@ async function fetchPost(postId){
     const content = document.getElementById("post-content");
     content.innerText = result.postBasic.contents;
 
-    const likes = document.getElementById("like-count");
-    likes.innerText = result.postCounter.likes;
+    //좋아요
+    isLike = result.isLike;
 
+    const likes = document.getElementById("post-like");
+    likes.querySelector("#like-count").innerText = result.postCounter.likes;
+
+    if (isLike) likes.setAttribute("active", "");
+    else likes.removeAttribute("active");
 
     const comments = document.getElementById("comment-count");
     comments.innerText = result.postCounter.comments;
 
     const views = document.getElementById("view-count");
     views.innerText = result.postCounter.views;
+    
+    if(!result.poster.isMe){
+      document.getElementById("post-modify-button").style.visibility = "hidden";
+      document.getElementById("post-delete-button").style.visibility = "hidden";
+    }
 
     sessionStorage.setItem("post", JSON.stringify(result));
-
   }catch(e){
 
   }
+
+
+  likes.addEventListener("click", async () =>{
+
+    if(isLoading) return;
+    isLoading = true;
+      if(!isLike){       
+        try{
+          const response = await postApi.postLike(postId);
+
+          if(!response.ok){
+            if(response.status === 409){
+              alert("이미 좋아요한 게시물입니다.");
+            }else{
+              alert("좋아요 실패");
+            }
+            return;
+          }
+
+          likes.setAttribute("active", "");
+          isLike = !isLike;
+          window.location.reload();
+        }catch(e){
+          alert("좋아요 실패");
+        }finally{
+          isLoading = false;
+        }
+      }else{
+        try{
+          const response = await postApi.deleteLike(postId);
+
+          if(!response.ok){
+            if(response.status === 404){
+              alert("좋아요 정보를 찾을 수 없습니다.");
+            
+            }else{
+              alert("좋아요 취소 실패");
+            }
+            return;
+          }
+          likes.removeAttribute("active");
+          isLike = !isLike;
+
+          window.location.reload();
+          
+        }catch(e){
+          alert("좋아요 취소 실패");
+        }finally{
+          isLoading = false;
+        }
+      }
+    });
+    
 }
 
-async function fetchComments(postId){
+async function fetchComments(){
   isLoading = true;
 
   try{
     const response = await commentApi.getComments({postId: postId, lastSeenId: lastSeenId});
-
     if(!response.ok){
       alert("댓글 조회 실패");
       return;
     }
 
     const result = await response.json();
-    
+   
     if (!result.comments || result.comments.length === 0) {
       fetchComplete = true;
       return;
@@ -214,25 +302,31 @@ async function renderComments(comments){
       contents: comment.commentBasic.contents
     });
 
+    if(!comment.poster.isMe){
+      wrapper.querySelector("#comment-modify-button").style.visibility = "hidden";
+      wrapper.querySelector("#comment-delete-button").style.visibility = "hidden";
+    }else{
+      wrapper.querySelector("#comment-modify-button").addEventListener("click",() => {
+        const commentText = document.querySelector("#comment-text");
+
+        commentText.value = comment.commentBasic.contents;
+        sessionStorage.setItem("commentId", comment.commentBasic.commentId);
+
+      });
+
+      wrapper.querySelector("#comment-delete-button").addEventListener("click", async () => {
+
+        try{
+          await commentApi.deleteComment({postId: postId, commentId: comment.commentBasic.commentId});
+          window.location.reload();
+
+        }catch(e){
+
+        }
+      });
+    }
+
     wrapper.dataset.commentId = comment.commentBasic.commentId;
     wrapper.dataset.userId = comment.poster.userId;    
-
-  });
-}
-
-//TODO
-async function addEventListenerToCommentCard() {
-
-}
-
-async function addEventListenerToLikeButton(postId) {
-  const likeButton = document.getElementById("post-like");
-
-  likeButton.addEventListener("click",()=>{
-    try{ 
-
-    }catch(e){
-
-    }
   });
 }
